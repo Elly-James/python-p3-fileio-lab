c5fcc914dce729b47a00310fb63eb5a5b4bc8edd
Elly <ellyjames1999@gmail.com>
2025-02-18 18:02:11 +0300

    done all the tests for file i/o

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+
+    with open(f"{file_name}.txt", mode='w', encoding= 'utf-8') as f:
+      f.write("This is a test content.")
+    
+    with open(f"{file_content}.txt", mode='w', encoding= 'utf-8') as f2:
+      f2.write("SmokingMirror")
+
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a', encoding= 'utf-8') as f3:
+      f3.write(append_content)
+    
+    
+    
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode='r', encoding= 'utf-8') as f4:
+      return f4.read()
